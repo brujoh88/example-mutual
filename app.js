af38f4a60b424dda62a88637c8ad3b6d31e1b4e8
Gustavo Tiseira <gustavotiseira@gmail.com>
2021-03-20 18:11:06 -0300

    esquema del proyecto - falta definir sistema de paginado/ruteo

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static(__dirname + '/public'))
+
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'lista_contactos',
+})
+
+connection.connect()
+let array
+connection.query(
+  'SELECT C.idContacto, C.nombre, C.email, C.telefono FROM contactos C',
+  function (error, results, fields) {
+    if (error) throw error
+    call(results)
+  }
+)
+const call = (dato) => {
+  array = dato
+}
+app.get('/getData', (req, res) => {
+  res.status(200).send(array)
+})
+
+app.listen(port, () =>
+  console.log(`La app esta escuando en la URL http://localhost:${port}`)
+)
